1c08164a54c923cde585acfa2807c791558958cb
avrhahm sason <avi6190i@gmail.com>
2024-09-10 12:59:15 +0300

    '.'

--- dist/cjs/helpers/login.helpers.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const convert_to_short_phone_number: (phone_number: string) => string;
-export declare const get_users_by_phone: (phone_number: string) => Promise<{
-    installer: any;
-    dashboard_user: any;
-}>;
--- dist/cjs/helpers/times_helpers.d.ts
+++
@@ -1,4 +0,0 @@
-import { FB_TIMESTAMP } from "../types/interfaces/global";
-export declare function timestamp_to_string(firebaseTimestamp: FB_TIMESTAMP): string;
-export declare function timestamp_to_millis(firebaseTimestamp: FB_TIMESTAMP): number;
-export declare function sort_by_timestamp(a: FB_TIMESTAMP, b: FB_TIMESTAMP, reverse?: boolean): number;
