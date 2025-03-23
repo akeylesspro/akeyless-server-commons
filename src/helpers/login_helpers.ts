5c25f222ccf3f5ff5d24e196e4aed83b73aa5edb
avrhahm sason <avi6190i@gmail.com>
2025-03-23 16:23:18 +0200

    support-login-by-email

--- src/helpers/login_helpers.ts
+++ src/helpers/login_helpers.ts
@@ -4,24 +4,19 @@ import { query_document, query_document_optional } from "./firebase_helpers";
 export const convert_to_short_phone_number = (phone_number: string): string => {
     return `0${phone_number.split("+972")[1]}`;
 };
-interface Users {
-    nx_user?: NxUser;
-}
-export const get_users_by_phone = async (phone_number: string, default_client_key: DefaultClient = DefaultClient.default): Promise<Users> => {
-    let nx_user;
 
-    const nx_user_q: NxUser | null = await query_document_optional("nx-users", "phone_number", "in", [
-        convert_to_short_phone_number(phone_number),
-        phone_number,
+export const get_user_by_identifier = async (identifier: string): Promise<NxUser> => {
+    const phone_query: NxUser | null = await query_document_optional("nx-users", "phone_number", "in", [
+        convert_to_short_phone_number(identifier),
+        identifier,
     ]);
-    if (!nx_user_q) {
-        throw "User not found";
-    }
 
-    if (nx_user_q) {
-        nx_user = nx_user_q;
+    if (!phone_query) {
+        const email_query: NxUser | null = await query_document_optional("nx-users", "email", "==", identifier);
+        if (!email_query) {
+            throw "User not found";
+        }
+        return email_query;
     }
-    return {
-        nx_user,
-    };
+    return phone_query;
 };
