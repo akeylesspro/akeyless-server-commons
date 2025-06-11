0bcc05c3b1a8c5efdeaa857070a61775ada40a15
avrhahm sason <avi6190i@gmail.com>
2025-06-11 15:31:11 +0300

    ././

--- src/helpers/firebase_helpers.ts
+++ src/helpers/firebase_helpers.ts
@@ -197,9 +197,9 @@ export const set_document = async (collection_path: string, doc_id: string, data
     }
 };
 
-export const add_document = async (collection_path: string, data: {}, include_id = false): Promise<void> => {
+export const add_document = async (collection_path: string, data: {}, include_id = false, custom_id?: string): Promise<void> => {
     try {
-        const new_document = db.collection(collection_path).doc();
+        const new_document = custom_id ? db.collection(collection_path).doc(custom_id) : db.collection(collection_path).doc();
         const update = include_id ? { ...data, id: new_document.id } : data;
         await new_document.set(update);
     } catch (error) {
